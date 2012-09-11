9aaee0150d58acc06e74a62939c58d84bca88dec
Ali Rizvi <aliabbasrizvi@gmail.com>
2012-09-11 03:45:11 -0700

    adding a background color and text label

--- app/controllers/istikhara_view_controller.rb
+++ app/controllers/istikhara_view_controller.rb
@@ -1,12 +1,14 @@
 class IstikharaViewController < UIViewController
   def loadView
-    #self.view = IstikharaView.alloc.init
+    self.view = UIView.alloc.init
     filePath = NSBundle.mainBundle.pathForResource("quran-uthmani", ofType:"txt")
-    @contents = File.open(filePath).readlines
+    @contents = File.open(filePath) { |file| file.readlines }
   end
 
   def viewDidLoad
     super
+
+    self.view.backgroundColor = UIColor.whiteColor
 
     alert = UIAlertView.new
     message = @contents[rand(6200)]
@@ -14,5 +16,12 @@ class IstikharaViewController < UIViewController
     alert.addButtonWithTitle("OK")
     alert.show
 
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = message
+    @label.sizeToFit
+    @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
+    self.view.addSubview @label
+
   end
+
 end
