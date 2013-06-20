8e12e438e1d7b6fb65befffc6ef75597ded344d5
Ali Rizvi <aliabbasrizvi@gmail.com>
2013-06-19 19:47:39 -0700

    added functionality to see previous and next ayat with  some refactoring

--- Rakefile
+++ Rakefile
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project'
-require 'bubble-wrap/all'
+#require 'bubble-wrap/all'
 
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
@@ -11,7 +11,7 @@ Motion::Project::App.setup do |app|
   app.deployment_target = "5.0"
   app.development do
     app.codesign_certificate = "iPhone Developer: Syed Rizvi (8PW637K7EM)"
-    app.provisioning_profile = "/Users/ali/Downloads/13-09-2012/Istikhara.mobileprovision"
+    app.provisioning_profile = "/Volumes/Data Drive/Dropbox/Dev/Istikhara.mobileprovision"
   end
 end
 
--- app/controllers/istikhara_view_controller.rb
+++ app/controllers/istikhara_view_controller.rb
@@ -12,31 +12,83 @@ class IstikharaViewController < UIViewController
 
     message = randomAyah
 
-    @textView = UITextView.alloc.initWithFrame(CGRectMake(50,80,220,240))
-    @textView.text = message
-    @textView.font = UIFont.systemFontOfSize(20)
-    @textView.textColor = UIColor.blackColor
-    @textView.editable = false
-    @textView.scrollEnabled = true
-    @textView.textAlignment = UITextAlignmentRight
-    @textView.layer.borderColor = UIColor.grayColor.CGColor
-    @textView.layer.borderWidth = 3.0
+    @textView = textView(message)
 
-    @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
-    @button.setTitle('Next', forState:UIControlStateNormal)
-    @button.addTarget(self, action:'nextTapped', forControlEvents:UIControlEventTouchUpInside)
-    @button.frame = [[50, 400], [view.frame.size.width - 50 * 2, 40]]
+    button_y = 400
+    button_height = 40
+
+    button_x = 50
+    button_width = 50
+    frame = [[button_x, button_y], [button_width, button_height]]
+    @prevButton = button('<<', 'previousAyat', frame)
+
+    button_x = 108
+    button_width = 100
+    frame = [[button_x, button_y], [button_width, button_height]]
+    @istikharaButton = button('Istikhara', 'nextTapped', frame)
+
+    button_x = 216
+    button_width = 50
+    frame = [[button_x, button_y], [button_width, button_height]]
+    @nextButton = button('>>', 'nextAyat', frame)
 
     self.view.addSubview @textView
-    self.view.addSubview @button
+    self.view.addSubview @istikharaButton
+    self.view.addSubview @prevButton
+    self.view.addSubview @nextButton
   end
 
   def nextTapped
     @textView.text = randomAyah
   end
 
+  def previousAyat
+    @ayatNumber -= 1
+    @textView.text = @contents[@ayatNumber]
+  end
+
+  def nextAyat
+    @ayatNumber += 1
+    @textView.text = @contents[@ayatNumber]
+  end
+
+  def button(title, action, frame)
+    button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    button.setTitle(title, forState:UIControlStateNormal)
+    button.addTarget(self, action:action, forControlEvents:UIControlEventTouchUpInside)
+    button.frame = frame
+    button
+  end
+
+  def alert(title, message, cancelButtonTitle="OK", delegate=nil, otherButtonTitle=nil)
+    UIAlertView.alloc.initWithTitle(title,
+      message: message,
+      delegate: delegate,
+      cancelButtonTitle: cancelButtonTitle,
+      otherButtonTitles: otherButtonTitle)
+  end
+
+  def textView(message)
+    x = 50
+    y = 80
+    width = 220
+    height = 300
+
+    textView = UITextView.alloc.initWithFrame(CGRectMake(x,y,width,height))
+    textView.text = message
+    textView.font = UIFont.systemFontOfSize(30)
+    textView.textColor = UIColor.blackColor
+    textView.editable = false
+    textView.scrollEnabled = true
+    textView.textAlignment = UITextAlignmentRight
+    textView.layer.borderColor = UIColor.grayColor.CGColor
+    textView.layer.borderWidth = 3.0    
+    textView
+  end
+
   def randomAyah
-    @contents[rand(6235)]
+    @ayatNumber = rand(6235)
+    @contents[@ayatNumber]
   end
 
 end
